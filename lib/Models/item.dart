530f880b78f86bcc6758b72a538f5a64f9df3d89
Caner KALE <95261394+kalecaner@users.noreply.github.com>
2022-05-18 19:27:43 +0300

    Create item and item repository

---
+++ lib/Models/item.dart
@@ -0,0 +1,29 @@
+import 'package:ne_nerede/Models/category.dart';
+import 'package:ne_nerede/Models/user.dart';
+
+class item {
+  String itemname;
+  Category category;
+  DateTime expirationDate;
+  DateTime supplyDate;
+  double Amount;
+  Unit unit;
+  String? brand;
+  String? subCategory;
+  String? description;
+  String Type;
+
+  item(this.itemname, this.category, this.expirationDate, this.supplyDate,
+      this.Amount, this.unit, this.brand, this.subCategory, this.Type);
+}
+enum Unit
+{
+ml,
+adet,
+  g,
+}
+class itemRepository{
+  List<item>  items= [
+    item("Süt",Category("Buzdolabı", User("caner", "kale", "a@gmail.com", "kalecaner", "12345", "derince")),DateTime(2022,05,21), DateTime.now(), 2000,Unit.ml , "Sütaş", "süt ürünleri", "içecek"),
+
+  ];}
